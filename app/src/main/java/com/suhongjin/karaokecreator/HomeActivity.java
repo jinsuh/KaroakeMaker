9c05b1a29650e72a68c399744bb304b75dda1f0d
Suhong Jin <suhongjin94@gmail.com>
2019-03-31 15:15:17 -0700

    Use androidx dependencies

--- app/src/main/java/com/suhongjin/karaokecreator/HomeActivity.java
+++ app/src/main/java/com/suhongjin/karaokecreator/HomeActivity.java
@@ -1,6 +1,6 @@
 package com.suhongjin.karaokecreator;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
